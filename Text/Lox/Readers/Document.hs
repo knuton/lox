20445923b80c9b098dd2b7d53dada0cde82fbb2b
Johannes Emerich <johannes@emerich.de>
2013-07-22 19:17:25 +0200

    Improve finding of document parts

--- Text/Lox/Readers/Document.hs
+++ Text/Lox/Readers/Document.hs
@@ -1,4 +1,4 @@
-module Text.Lox.Readers.Document (constituents) where
+module Text.Lox.Readers.Document (constituents, parse) where
 
 import Control.Applicative ((<$>), (<*>), (*>), (<*))
 import Text.ParserCombinators.Parsec
@@ -7,10 +7,14 @@ import Text.Lox.Parsing
 constituents :: Char -> GenParser Char st [Either String String]
 constituents sep = many1 (try (plain sep) <|> (loxStatement sep)) <* eof
 
+
 plain :: Char -> GenParser Char st (Either String String)
-plain sep = Left <$> (try plainTail <|> many1 (noneOf [sep]))
+plain sep = Left <$> (try plainTail <|> try (many1 (noneOf [sep])) <|> unmatched)
   where
-    plainTail = char sep *> many1 (noneOf [sep]) <* eof
+    plainTail =
+      (sep:) <$> (char sep *> many (noneOf [sep]) <* eof)
+    unmatched =
+      (\x -> (sep:x) ++ "\n") <$> (char sep *> many1 (noneOf [sep, '\n']) <* char '\n')
 
 loxStatement :: Char -> GenParser Char st (Either String String)
-loxStatement sep = Right <$> (char '§' *> many1 (noneOf [sep]) <* char '§')
+loxStatement sep = Right <$> (char sep *> many1 (noneOf [sep, '\n']) <* char sep)
